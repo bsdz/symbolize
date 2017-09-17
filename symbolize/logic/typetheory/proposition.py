c28acc362c49227b7cec27421ecb4ff4ae10b835
bsdz <bsdz@users.noreply.github.com>
2017-09-17 14:16:15 +0100

    refactor Proposition

--- symbolize/logic/typetheory/proposition.py
+++ symbolize/logic/typetheory/proposition.py
@@ -4,6 +4,7 @@ from ...expressions import ExpressionMetaClass, Expression, Symbol, BaseWithChil
         LogicQuantificationSymbol, LogicQuantificationExpression
         
 from .proof import ProofSymbol, ProofExpressionCombination
+from symbolize.utility import ToBeImplemented
 
 def general_proof_label_generator():
     from itertools import count
@@ -18,17 +19,11 @@ class PropositionExpressionMetaClass(ExpressionMetaClass):
 
 class PropositionExpression(Expression, metaclass=PropositionExpressionMetaClass):
     def __init__(self, *args, **kwargs):
-        self.proof_default_arity = kwargs.pop("proof_default_arity", None)
-        self.proof_function = kwargs.pop("proof_function", None)
         self.assumption_contains_free = kwargs.pop("assumption_contains_free", [])
         super().__init__()
         
     def get_proof(self, name):
-        if self.proof_function is not None:
-            new_proof = self.proof_function(self).alias(name)
-        else:
-            new_proof = ProofSymbol(str_repr=name, proposition_type=self, arity=self.proof_default_arity)
-        return new_proof
+        raise ToBeImplemented("Need to implement")
     
     def contains_free(self, expr):
         """Allow us to override base"""
@@ -37,9 +32,13 @@ class PropositionExpression(Expression, metaclass=PropositionExpressionMetaClass
         else:
             return super().contains_free(expr)
         
+    def apply(self, *expressions):
+        return super().apply(*expressions, application_kwargs={})
+        
 class PropositionSymbol(Symbol, metaclass=PropositionExpressionMetaClass, expression_base_class=PropositionExpression):
     """*@DynamicAttrs*"""
-    pass
+    def get_proof(self, name):
+        return ProofSymbol(str_repr=name, proposition_type=self)
 
 class PropositionBaseWithChildrenExpression(BaseWithChildrenExpression, metaclass=PropositionExpressionMetaClass, expression_base_class=PropositionExpression):
     pass
@@ -50,53 +49,61 @@ class PropositionBinaryInfixExpression(BinaryInfixExpression, metaclass=Proposit
 class PropositionBinaryInfixSymbol(BinaryInfixSymbol, metaclass=PropositionExpressionMetaClass, expression_base_class=PropositionExpression):
     __default_application_class__ = PropositionBinaryInfixExpression
         
-    def apply(self, *expressions):
-        return super().apply(*expressions, application_kwargs={
-            "proof_default_arity": self.proof_default_arity,
-            "proof_function": self.proof_function
-        })
-        
 class PropositionLogicQuantificationExpression(LogicQuantificationExpression, metaclass=PropositionExpressionMetaClass, expression_base_class=PropositionBaseWithChildrenExpression):
     pass
         
 class PropositionLogicQuantificationSymbol(LogicQuantificationSymbol, metaclass=PropositionExpressionMetaClass, expression_base_class=PropositionExpression):
     __default_application_class__ = PropositionLogicQuantificationExpression
         
-    def apply(self, *expressions):
-        return super().apply(*expressions, application_kwargs={
-            "proof_default_arity": self.proof_default_arity,
-            "proof_function": self.proof_function
-        })
-
-
-def and_pf(prop_type):
-    p1 = prop_type.children[0].get_proof(next(proof_label_generator))
-    p2 = prop_type.children[1].get_proof(next(proof_label_generator))
-    return ProofExpressionCombination(p1, p2)
-
-def implies_pf(prop_type):
-    p1 = prop_type.children[0].get_proof(next(proof_label_generator))
-    p2 = prop_type.children[1].get_proof(next(proof_label_generator))
-    return p2.abstract(p1)
-
-def or_pf(prop_type):
-    from .proof import inl, inr
-    p1 = prop_type.children[0].get_proof(next(proof_label_generator))
-    p2 = prop_type.children[1].get_proof(next(proof_label_generator))
-    # todo - can be inl or inr with indicator of which proof has evidence
-    return inl(p1, p2.proposition_type)
 
 # definitions
 #
-and_ = PropositionBinaryInfixSymbol('∧', latex_repr=r'\land', proof_default_arity=ArityCross(A0,A0), proof_function=and_pf)
-or_ = PropositionBinaryInfixSymbol('∨', latex_repr=r'\lor', proof_default_arity=ArityCross(A0,A0), proof_function=or_pf)
-implies = PropositionBinaryInfixSymbol('⟹', latex_repr=r'\Rightarrow', proof_default_arity=ArityArrow(A0,A0), proof_function=implies_pf)
+class AndPropositionExpression(PropositionBinaryInfixExpression):
+    def get_proof(self, name):
+        p1 = self.children[0].get_proof(next(proof_label_generator))
+        p2 = self.children[1].get_proof(next(proof_label_generator))
+        return ProofExpressionCombination(p1, p2).alias(name)
+        
+class AndPropositionSymbol(PropositionBinaryInfixSymbol):
+    __default_application_class__ = AndPropositionExpression
+    
+class OrPropositionExpression(PropositionBinaryInfixExpression):
+    def get_proof(self, name):
+        from .proof import inl, inr
+        p1 = self.children[0].get_proof(next(proof_label_generator))
+        p2 = self.children[1].get_proof(next(proof_label_generator))
+        # todo - can be inl or inr with indicator of which proof has evidence
+        return inl(p1, p2.proposition_type).alias(name)
+        
+class OrPropositionSymbol(PropositionBinaryInfixSymbol):
+    __default_application_class__ = OrPropositionExpression
+    
+class ImpliesPropositionExpression(PropositionBinaryInfixExpression):
+    def get_proof(self, name):
+        p1 = self.children[0].get_proof(next(proof_label_generator))
+        p2 = self.children[1].get_proof(next(proof_label_generator))
+        return p2.abstract(p1).alias(name)
+        
+class ImpliesPropositionSymbol(PropositionBinaryInfixSymbol):
+    __default_application_class__ = ImpliesPropositionExpression
+    
+class ForallPropositionExpression(PropositionLogicQuantificationExpression):
+    def get_proof(self, name):
+        p1 = self.children[0].get_proof(next(proof_label_generator))
+        p2 = self.children[1].get_proof(next(proof_label_generator))
+        return p2.abstract(p1).alias(name)
+        
+class ForallPropositionSymbol(PropositionLogicQuantificationSymbol):
+    __default_application_class__ = ForallPropositionExpression
 
-forall = PropositionLogicQuantificationSymbol('∀', latex_repr=r'\forall')
+
+and_ = AndPropositionSymbol('∧', latex_repr=r'\land')
+or_ = OrPropositionSymbol('∨', latex_repr=r'\lor')
+implies = ImpliesPropositionSymbol('⟹', latex_repr=r'\Rightarrow')
+forall = ForallPropositionSymbol('∀', latex_repr=r'\forall')
 
 #then = PropositionBinaryInfixSymbol('⟸', latex_repr=r'\Leftarrow')
 #iff = PropositionBinaryInfixSymbol('⟺', latex_repr=r'\iff')
 
 not_ = PropositionSymbol('¬', arity=ArityArrow(A0,A0), latex_repr=r'\neg')
 #exists = LogicQuantificationSymbol('∃', latex_repr=r'\exists')
-
